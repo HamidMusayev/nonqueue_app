86d67c6ad797d2a6c9e9615384d194a9e9c71002
Hamidvs24 <hemidvsmmusayev@gmail.com>
2022-02-21 17:29:42 +0400

    commit

--- lib/screens/checkout/ui.dart
+++ lib/screens/checkout/ui.dart
@@ -70,179 +70,189 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
               color: Colors.white,
               borderRadius: Radiuses.r20,
             ),
-            child: Stepper(
-              onStepContinue: () => setState(() => _activeStep++),
-              onStepCancel: () => setState(() => _activeStep--),
-              currentStep: _activeStep,
-              physics: const BouncingScrollPhysics(),
-              steps: [
-                Step(
-                  isActive: _activeStep == 0,
-                  state:
-                      _activeStep > 0 ? StepState.complete : StepState.indexed,
-                  title: const Text('Details'),
-                  content: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      RadioListTile(
-                        title: const Text('Pickup'),
-                        subtitle: const Text('I’ll take the food with myself.'),
-                        value: 1,
-                        groupValue: _pickedType,
-                        onChanged: (int? value) =>
-                            setState(() => _pickedType = value),
-                      ),
-                      RadioListTile(
-                        title: const Text('On site'),
-                        subtitle: const Text('I’ll it at restaraunt.'),
-                        value: 2,
-                        groupValue: _pickedType,
-                        onChanged: (int? value) =>
-                            setState(() => _pickedType = value),
-                      ),
-                      Spaces.vertical10,
-                      ListTile(
-                        title: const Text('Your arrival time'),
-                        subtitle: Text(
-                          _pickedTime.format(context),
-                          style: const TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.w500,
-                            color: ColorPalette.greyInputText,
-                          ),
+            child: Theme(
+              data: Theme.of(context).copyWith(
+              ),
+              child: Stepper(
+                onStepContinue: () => setState(() => _activeStep++),
+                onStepCancel: () => setState(() => _activeStep--),
+                currentStep: _activeStep,
+                physics: const BouncingScrollPhysics(),
+                steps: [
+                  Step(
+                    isActive: _activeStep == 0,
+                    state:
+                        _activeStep > 0 ? StepState.indexed : StepState.indexed,
+                    title: const Text('Details'),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        RadioListTile(
+                          title: const Text('Pickup'),
+                          subtitle:
+                              const Text('I’ll take the food with myself.'),
+                          value: 1,
+                          groupValue: _pickedType,
+                          onChanged: (int? value) =>
+                              setState(() => _pickedType = value),
                         ),
-                        leading: OutlinedButton(
-                          style: OutlinedButton.styleFrom(
-                            fixedSize: const Size.fromHeight(45),
-                            shape: const RoundedRectangleBorder(
-                              borderRadius: Radiuses.r10,
+                        RadioListTile(
+                          title: const Text('On site'),
+                          subtitle: const Text('I’ll it at restaraunt.'),
+                          value: 2,
+                          groupValue: _pickedType,
+                          onChanged: (int? value) =>
+                              setState(() => _pickedType = value),
+                        ),
+                        Spaces.vertical10,
+                        ListTile(
+                          title: const Text('Your arrival time'),
+                          subtitle: Text(
+                            _pickedTime.format(context),
+                            style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w500,
+                              color: ColorPalette.greyInputText,
                             ),
                           ),
-                          child: const Icon(Icons.access_time_rounded),
-                          onPressed: () async => await _selectTime(),
+                          leading: OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                              fixedSize: const Size.fromHeight(45),
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: Radiuses.r10,
+                              ),
+                            ),
+                            child: const Icon(Icons.access_time_rounded),
+                            onPressed: () async => await _selectTime(),
+                          ),
                         ),
-                      ),
-                      Spaces.vertical20,
-                      Spaces.vertical10,
-                    ],
+                        Spaces.vertical20,
+                        Spaces.vertical10,
+                      ],
+                    ),
                   ),
-                ),
-                Step(
-                  isActive: _activeStep == 1,
-                  state:
-                      _activeStep > 1 ? StepState.complete : StepState.indexed,
-                  title: const Text('Payment'),
-                  content: Column(
-                    children: [
-                      RadioListTile(
-                        title: const Text('N-Q balance: 35.00 AZN'),
-                        value: 0,
-                        groupValue: _pickedPayment,
-                        onChanged: (int? value) =>
-                            setState(() => _pickedPayment = value),
-                      ),
-                      ..._cards.map(
-                        (c) => RadioListTile(
-                          title: Text(c['name']),
+                  Step(
+                    isActive: _activeStep == 1,
+                    state: _activeStep > 1
+                        ? StepState.complete
+                        : StepState.indexed,
+                    title: const Text('Payment'),
+                    content: Column(
+                      children: [
+                        RadioListTile(
+                          title: const Text('N-Q balance: 35.00 AZN'),
+                          value: 0,
+                          groupValue: _pickedPayment,
                           onChanged: (int? value) =>
                               setState(() => _pickedPayment = value),
-                          value: c['value'] as int,
-                          groupValue: _pickedPayment,
-                          subtitle: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(c['number']),
-                              SvgPicture.asset(c['icon'], height: 12),
-                            ],
-                          ),
                         ),
-                      ),
-                      Spaces.vertical20,
-                      Spaces.vertical10,
-                    ],
-                  ),
-                ),
-                Step(
-                  isActive: _activeStep == 2,
-                  state:
-                      _activeStep > 2 ? StepState.complete : StepState.indexed,
-                  title: const Text('Confirmation'),
-                  content: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SvgPicture.asset(
-                        'assets/splash/check.svg',
-                        height: 60,
-                      ),
-                      Spaces.horizontal6,
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text('Congratulations!'),
-                          Text('Your order has successfully done!'),
-                          Spaces.vertical10,
-                          Text(
-                            'Your order number is',
-                            style: TextStyle(fontWeight: FontWeight.w500),
-                          ),
-                          Text(
-                            '#241218',
-                            style: TextStyle(
-                              color: ColorPalette.greyInputText,
-                              fontWeight: FontWeight.w500,
-                              fontSize: 26,
+                        ..._cards.map(
+                          (c) => RadioListTile(
+                            title: Text(c['name']),
+                            onChanged: (int? value) =>
+                                setState(() => _pickedPayment = value),
+                            value: c['value'] as int,
+                            groupValue: _pickedPayment,
+                            subtitle: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(c['number']),
+                                SvgPicture.asset(c['icon'], height: 12),
+                              ],
                             ),
                           ),
-                          Spaces.vertical50,
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-              controlsBuilder: (context, controlDetails) => Row(
-                children: [
-                  Visibility(
-                    visible: _activeStep == 2,
-                    child: Container(),
-                    replacement: TextButton(
-                      onPressed: controlDetails.onStepContinue,
-                      style: TextButton.styleFrom(
-                          fixedSize: const Size.fromHeight(45)),
-                      child: Padding(
-                        padding: Paddings.p24.copyWith(top: 0, bottom: 0),
-                        child: const Text('Next'),
-                      ),
+                        ),
+                        Spaces.vertical20,
+                        Spaces.vertical10,
+                      ],
                     ),
                   ),
-                  Visibility(
-                    visible: _activeStep == 2,
-                    child: TextButton(
-                      onPressed: controlDetails.onStepCancel,
-                      style: TextButton.styleFrom(
-                          fixedSize: const Size.fromHeight(45)),
-                      child: Padding(
-                        padding: Paddings.p24.copyWith(top: 0, bottom: 0),
-                        child: const Text('Track order'),
-                      ),
-                    ),
-                  ),
-                  Spaces.horizontal6,
-                  Visibility(
-                    visible: _activeStep == 1,
-                    replacement: Container(),
-                    child: TextButton(
-                      onPressed: controlDetails.onStepCancel,
-                      style: TextButton.styleFrom(
-                          backgroundColor: ColorPalette.greyInputText,
-                          fixedSize: const Size.fromHeight(45)),
-                      child: Padding(
-                        padding: Paddings.p24.copyWith(top: 0, bottom: 0),
-                        child: const Text('Back'),
-                      ),
+                  Step(
+                    isActive: _activeStep == 2,
+                    state: _activeStep > 2
+                        ? StepState.complete
+                        : StepState.indexed,
+                    title: const Text('Confirmation'),
+                    content: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SvgPicture.asset(
+                          'assets/splash/check.svg',
+                          height: 60,
+                        ),
+                        Spaces.horizontal6,
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text('Congratulations!'),
+                            Text('Your order has successfully done!'),
+                            Spaces.vertical10,
+                            Text(
+                              'Your order number is',
+                              style: TextStyle(fontWeight: FontWeight.w500),
+                            ),
+                            Text(
+                              '#241218',
+                              style: TextStyle(
+                                color: ColorPalette.greyInputText,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 26,
+                              ),
+                            ),
+                            Spaces.vertical50,
+                          ],
+                        ),
+                      ],
                     ),
                   ),
                 ],
+                controlsBuilder: (context, controlDetails) => Row(
+                  children: [
+                    Visibility(
+                      visible: _activeStep == 2,
+                      child: Container(),
+                      replacement: TextButton(
+                        onPressed: controlDetails.onStepContinue,
+                        style: TextButton.styleFrom(
+                            fixedSize: const Size.fromHeight(45)),
+                        child: Padding(
+                          padding: Paddings.p24.copyWith(top: 0, bottom: 0),
+                          child: const Text('Next'),
+                        ),
+                      ),
+                    ),
+                    Visibility(
+                      visible: _activeStep == 2,
+                      child: TextButton(
+                        onPressed: controlDetails.onStepCancel,
+                        style: TextButton.styleFrom(
+                            fixedSize: const Size.fromHeight(45)),
+                        child: Padding(
+                          padding: Paddings.p24.copyWith(top: 0, bottom: 0),
+                          child: const Text('Track order'),
+                        ),
+                      ),
+                    ),
+                    Spaces.horizontal6,
+                    Visibility(
+                      visible: _activeStep == 1,
+                      replacement: Container(),
+                      child: OutlinedButton(
+                        onPressed: controlDetails.onStepCancel,
+                        style: OutlinedButton.styleFrom(
+                            shape: const RoundedRectangleBorder(
+                                borderRadius: Radiuses.r10),
+                            primary: ColorPalette.lightBlack,
+                            side: BorderSide(color: ColorPalette.greyInputText),
+                            fixedSize: const Size.fromHeight(45)),
+                        child: Padding(
+                          padding: Paddings.p24.copyWith(top: 0, bottom: 0),
+                          child: const Text('Back'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           )
